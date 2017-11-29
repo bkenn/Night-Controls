28862dfd124e255264e4b378c4dff0c10222dbe3
brian <briankennedy392@gmail.com>
2017-11-28 20:45:04 -0500

    0.0.2 added function to add View directly

--- src/main/kotlin/us/nightware/controls/DSL.kt
+++ src/main/kotlin/us/nightware/controls/DSL.kt
@@ -6,19 +6,26 @@ import tornadofx.*
 import us.nightware.controls.card.Card
 import us.nightware.controls.card.CardHolder
 
-fun EventTarget.cardholder(op: (VBox.() -> Unit)? = null) : CardHolder {
+fun EventTarget.cardholder(op: (VBox.() -> Unit) = {}) : CardHolder {
     val cardHolder = CardHolder()
     val vb = vbox {
         paddingBottom = 100
         paddingHorizontal = 25
         paddingTop = 25
     }
-    op?.invoke(vb)
+    op.invoke(vb)
     cardHolder.add(vb)
     return opcr(this,cardHolder)
 }
 
-fun EventTarget.card(op: (Card.() -> Unit)? = null) : Card {
+fun EventTarget.card(op: (Card.() -> Unit) = {}) : Card {
     val card = Card()
     return opcr(this, card ,op)
 }
+
+inline fun <reified T: UIComponent> EventTarget.card(uiComponent: T, noinline op: (Card.() -> Unit) = {}): Card {
+    val card = Card()
+    card.add(find<T>().root)
+    return opcr(this, card ,op)
+}
+
